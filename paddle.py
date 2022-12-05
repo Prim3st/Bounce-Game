33a7960a374836dc03f2a4dc7d05ee00192af9f9
prime <prim3stskeletron@gmail.com>
2022-12-05 11:05:23 +0000

    Added ability to pause game with enter key

--- paddle.py
+++ paddle.py
@@ -9,6 +9,7 @@ class Paddle:
         self.canvas_width = canvas.winfo_width()
         self.canvas.bind_all("<Left>", self.turn_left)
         self.canvas.bind_all("<Right>", self.turn_right)
+        self.canvas.bind_all("<Return>", self.pauser)
         # TODO Pause the game, HINT: something similar as the line above can be done.
         
         
@@ -29,7 +30,7 @@ class Paddle:
     def turn_right(self, event):
         self.x = 3.5
 
-    def pauser(self,event):
+    def pauser(self, event):
         self.pausec+=1
         if self.pausec==2:
             self.pausec=0
